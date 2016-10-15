f42261fa73ca797551eec8d6c68a448454115a36
guyong <guyong@zhixueyun.com>
2016-10-15 19:14:50 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# VS Code Sleet
+
+[Sleet](https://github.com/JacoKoo/sleetjs) and [Sleet Handlebars](https://github.com/JacoKoo/sleet-handlebars)
+plugin for [VS Code](code.visualstudio.com)
+
+## Features
+
+* Grammar highlight
+* Compile on save
+
+## Extension Commands
+
+
+* `extension.sleet.compile`: compile current sleet file
---
+++ extension.js
@@ -0,0 +1,63 @@
+const {window, workspace, commands} = require('vscode');
+const fs = require('fs');
+const path = require('path');
+const {compile} = require('sleet');
+
+const hasCompiledFile = (file) => {
+    const dir = path.dirname(file);
+    const filename = path.basename(file);
+    const name = path.basename(file, path.extname(file));
+    return fs.readdirSync(dir).filter(f => f !== filename && path.basename(f, path.extname(f)) === name).length !== 0;
+};
+const getPackageConfig = (file) => {
+    let root = path.resolve(path.dirname(file));
+
+    while (path.dirname(root) !== root) {
+        const pkg = path.join(root, 'package.json');
+        if (fs.existsSync(pkg)) {
+            try {
+                const obj = JSON.parse(fs.readFileSync(pkg));
+                if (obj.sleet) return obj.sleet;
+            } catch (e) {
+                // ignore
+            }
+        }
+        root = path.dirname(root);
+    }
+    return null;
+};
+const compileIt = (content, file) => {
+    const options = getPackageConfig(file) || {};
+    options.filename = file;
+    return compile(content, options);
+};
+
+let doCompile = (document, force) => {
+    if (document.languageId !== 'sleet') return;
+
+    const file = document.fileName;
+    if (!force && !hasCompiledFile(file)) return;
+
+    const compiled = compileIt(document.getText(), file);
+
+    const target = `${path.join(path.dirname(file), path.basename(file, path.extname(file)))}.${compiled.extension}`;
+    fs.writeFileSync(target, compiled.content, 'utf-8');
+};
+
+function activate(context) {
+    context.subscriptions.push(workspace.onDidChangeTextDocument((e) => {
+        doCompile(e.document);
+    }));
+
+    let disposable = commands.registerCommand('extension.sleet.compile', () => {
+        let editor = window.activeTextEditor;
+        if (!editor || !editor.document) return;
+
+        doCompile(editor.document, true);
+    });
+
+    context.subscriptions.push(disposable);
+}
+exports.activate = activate;
+
+exports.deactivate = function() {};
